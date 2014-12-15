b3e87e184a275ba5c3432c763c7632706d543035
Liam Marshall <archimedespi3141@gmail.com>
2014-12-14 21:48:03 -0600

    Change #new to #initialize.
    Need to brush up on Ruby :crying_cat_face:

--- lib/gcodify/coordinates.rb
+++ lib/gcodify/coordinates.rb
@@ -6,12 +6,20 @@ module GCodify
 		# For later.
 		# attr_accessor :a
 
-		def new(x, y, z)
+		# #initialize method on Coordinate for making a new coordinate.
+		#
+		# @param x [Float] x coordinate
+		# @param y [Float] y coordinate
+		# @param z [Float] z coordinate
+		# @return 
+		def initialize(x, y, z)
 			@x = x
 			@y = y
 			@z = z
 		end
 
+		# Converts the set of coordinates to a GCode string in the form
+		# X[x] Y[y] Z[z].
 		def to_gcode_coordinates
 			return "X#{@x} Y#{@y} Z#{@z}"
 		end
