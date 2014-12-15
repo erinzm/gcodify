ee2a7606a0cc272e8c7c304123c8aa97427941a9
Liam Marshall <archimedespi3141@gmail.com>
2014-12-14 22:32:51 -0600

    Travis.yml and coordinate spec.

---
+++ spec/coordinate_spec.rb
@@ -0,0 +1,10 @@
+require 'gcodify/coordinates'
+
+RSpec.describe GCodify::Coordinate do
+	describe '#to_gcode_coordinates' do
+		it "converts the coodrinates to gcode X[x] Y[y] Z[z]" do
+			coordinate = GCodify::Coordinate.new(1, 2, 3)
+			expect(coordinate.to_gcode_coordinates).to eq('X1 Y2 Z3')
+		end
+	end
+end
