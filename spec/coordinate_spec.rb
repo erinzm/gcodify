47ba6103aece374b54f4ec91cf1f7c03fc9e2641
Liam M <ArchimedesPi@users.noreply.github.com>
2015-03-24 15:53:11 -0500

    Update coordinate_spec.rb

--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -2,7 +2,7 @@ require 'gcodify/coordinates'
 
 RSpec.describe GCodify::Coordinate do
 	describe '#to_gcode_coordinates' do
-		it "converts the coodinates to gcode X[x] Y[y] Z[z]" do
+		it "converts the coordinates to gcode X[x] Y[y] Z[z]" do
 			coordinate = GCodify::Coordinate.new(1, 2, 3)
 			expect(coordinate.to_gcode_coordinates).to eq('X1 Y2 Z3')
 		end
