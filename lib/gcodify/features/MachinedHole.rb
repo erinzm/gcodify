c75b19a30730e18d471f6084ff75df9951ab658a
Liam Marshall <archimedespi3141@gmail.com>
2015-04-12 19:52:49 -0500

    Populate MachinedHole; create MachinedHoleBuilder

--- lib/gcodify/features/MachinedHole.rb
+++ lib/gcodify/features/MachinedHole.rb
@@ -1,6 +1,25 @@
 module GCodify
 	module Features
 		class MachinedHole
+			attr_accessor :diameter
+			attr_accessor :depth
+
+			def initialize(diameter, depth)
+				@diameter = diameter
+				@depth = depth
+			end
+		end
+		class MachinedHoleBuilder
+			def diameter(diameter)
+				@diameter = diameter
+			end
+			def depth(depth); @depth = depth; end
+			def build
+				MachinedHole.new(@diameter, @depth)
+			end
+		end
+		def machined_hole(&block)
+			Docile.ds_eval(MachinedHoleBuilder.new, &block).build
 		end
 	end
 end
