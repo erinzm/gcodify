d300a1ff42aa612c8cdd32f5745cd80f30c35c99
Liam Marshall <archimedespi3141@gmail.com>
2015-04-12 19:52:58 -0500

    Test MachinedHole

---
+++ spec/machinedhole_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'gcodify/features/MachinedHole.rb'
+
+RSpec.describe GCodify::Features::MachinedHole do
+	describe '#new' do
+		it 'returns a new instance of MachinedHole' do
+			machinedhole = GCodify::Features::MachinedHole.new(10, 10)
+			expect(machinedhole).to be_instance_of(GCodify::Features::MachinedHole)
+		end
+	end
+	describe '#diameter' do
+		it 'returns the diameter of the hole' do
+			machinedhole = GCodify::Features::MachinedHole.new(10, 20)
+			expect(machinedhole.diameter).to eq(10)
+		end
+	end
+	describe '#depth' do
+		it 'returns the depth of the hole' do
+			machinedhole = GCodify::Features::MachinedHole.new(10, 20)
+			expect(machinedhole.depth).to eq(20)
+		end
+	end
+end
