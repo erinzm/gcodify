689c3a82564d123f8d146f3e13d233df551703de
Liam Marshall <liam@cpan.org>
2015-06-19 17:58:12 -0500

    Document Program

--- lib/gcodify/program.rb
+++ lib/gcodify/program.rb
@@ -1,10 +1,13 @@
 require "gcodify/opsmixin"
 
 module Gcodify
+  # Represents a GCode program. It contains a property @ops, which is an array
+  # of all the ops, in order, in the program.
   class Program
     attr_accessor :ops
+    # Mix in all the op functions for the DSL :D
     include OpsMixin
-
+    
     def initialize
       @ops = []
     end
@@ -15,9 +18,10 @@ module Gcodify
     def to_gcode
       gcode = ""
       @ops.each do |op|
+        # Append that Op's generated gcode to the full gcode string
         gcode << op.to_gcode
       end
-      gcode
+      gcode # Return the generated gcode
     end
   end
 end
