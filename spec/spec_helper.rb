5d5a540ab0688ee3e0af18251896b00e19a0b555
Liam Marshall <liam@cpan.org>
2015-06-19 14:17:07 -0500

    *cough* *cough* RTFM, says AT THE VERY TOP, puts it at the bottom :P

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'gcodify'
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'gcodify'
