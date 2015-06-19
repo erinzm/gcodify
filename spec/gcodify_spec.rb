ec44a5bed0662389776e51550a1088ef524ed38a
Liam Marshall <liam@cpan.org>
2015-06-19 17:46:27 -0500

    Add in new tests

--- spec/gcodify_spec.rb
+++ spec/gcodify_spec.rb
@@ -4,10 +4,16 @@ describe Gcodify do
   it 'has a version number' do
     expect(Gcodify::VERSION).not_to be nil
   end
+  it 'works correctly with a dummy operation' do
+    program = Gcodify.program do
+      dummy "foo"
+    end
+    expect(program.to_gcode).to eq "foo"
+  end
   it 'does useful work' do
     program = Gcodify.program do
       hole :at => [10, 10], :depth => 10
     end
-    expect(program.to_gcode).not_to be nil
+    expect(program.to_gcode).not_to eq ""
   end
 end
--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -4,6 +4,6 @@ describe Gcodify::Program do
   it 'converts ops to gcode' do
     program = Gcodify::Program.new
     program.ops = [Gcodify::Ops::DummyOp.new("asdf")]
-    expect(program.to_gcode).to eq("asdf");
+    expect(program.to_gcode).to eq "asdf";
   end
 end
