cbf30762daf71e5c22f17ebff94ca46772e3b9ac
Liam Marshall <liam@cpan.org>
2015-06-19 15:27:58 -0500

    Document GCodify.program

--- lib/gcodify.rb
+++ lib/gcodify.rb
@@ -2,6 +2,14 @@ require "gcodify/version"
 require "gcodify/program"
 
 module Gcodify
+  # Takes a block of operations and creates a new {Program} from them.
+  #
+  # @example Using it to create a {Program}
+  #   Gcodify.program do
+  #     hole :at=>[0, 0], :depth=>0.25
+  #   end
+  # @param &block [Block] a block of operations (see Ops).
+  # @return [Program] a program created from `&block`
   def self.program(&block)
     prog = Program.new
     prog.instance_eval(&block)
