5dd33cb5db12685b283083eff584a1155aaea18d
Liam Marshall <liam@cpan.org>
2015-06-19 15:37:45 -0500

    Document GCodify::Ops

--- lib/gcodify/ops.rb
+++ lib/gcodify/ops.rb
@@ -1,3 +1,7 @@
+# {Ops} is used as a Mixin to {Program} for the DSL - all of the DSL's methods,
+# ie. hole, pocket, face, etc. are implemented here, and then `import`ed into
+# {Program}, for DRYness and reusability.
+# All methods contained in {Ops} return a subclass of Op, ie. HoleOp, PocketOp, and so on.
 module Gcodify::Ops
   # options is a Hash
   # standard stuff in options for all these functions
@@ -8,6 +12,18 @@ module Gcodify::Ops
   # :diameter - diameter of the feature, behavior is undefined when specified at the same time as :radius
   # :peck - peck step depth for peck drilling
   # :return_height - return height for pecking during peck drilling. if not specified, pecking will not return to a constant height
+  #
+  # @return [HoleOp] an operation
   def hole(options)
+  end
+
+  # (see #hole)
+  # @return [PocketOp] an operation
+  def pocket(options)
+  end
+
+  # (see #hole)
+  # @return [FaceOp] an operation
+  def face(options)
   end
 end
