4fe228fe74fe69d5b08a6bc2e931f9b2f6555fa2
Liam Marshall <liam@cpan.org>
2015-06-19 17:53:14 -0500

    Document Op

--- lib/gcodify/op.rb
+++ lib/gcodify/op.rb
@@ -1,6 +1,15 @@
 module Gcodify
+  # An Operation. An Operation is the unit of "doing". For instance, a drilled hole
+  # is a single {Op}, a circular pocket is a single {Op}, and so on.
+  # {Op}s are subclassed into things like HoleOp, PocketOp, etc., and each is responsible
+  # for storing data and each provides its own implementation of #to_gcode.
   class Op
+    # Converts the {Op} to Gcode. Each *{Op} has it's own implementation of #to_gcode,
+    # depending on what gcode it has to generate.
+    #
+    # @return [String] some gcode
     def to_gcode
+      "" # Return an empty string for testing purposes.
     end
   end
 end
