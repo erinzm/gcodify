163ac52d6de77af5d5a13e5523b8705a5b80bd74
Liam Marshall <liam@cpan.org>
2015-06-19 15:30:22 -0500

    Implement Program.to_gcode

--- lib/gcodify/program.rb
+++ lib/gcodify/program.rb
@@ -2,8 +2,14 @@ require "gcodify/ops"
 
 module Gcodify
   class Program
+    :attr_reader ops
     include Ops
+
     def to_gcode
+      gcode = ""
+      @ops.each do |op|
+        gcode << op.to_gcode
+      end
     end
   end
 end
