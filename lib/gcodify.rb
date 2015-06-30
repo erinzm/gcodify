9ac51e4b3f69ebab20f50734ae00f76d6d8423d0
Liam Marshall <liam@cpan.org>
2015-06-30 11:07:46 -0500

    Fix doc

--- lib/gcodify.rb
+++ lib/gcodify.rb
@@ -12,7 +12,7 @@ module Gcodify
   #   Gcodify.program do
   #     hole :at=>[0, 0], :depth=>0.25
   #   end
-  # @param &block [Block] a block of operations (see Ops).
+  # @param block [Block] a block of operations (see Ops).
   # @return [Program] a program created from `&block`
   def self.program(&block)
     prog = Program.new
