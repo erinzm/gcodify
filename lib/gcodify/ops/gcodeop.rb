bbf6a7a5446f599e7fba386c60bb4719d8b6fc79
Liam Marshall <liam@cpan.org>
2015-06-30 12:16:46 -0500

    Document GcodeOp, make GcodeOp#to_gcode append '\n'

--- lib/gcodify/ops/gcodeop.rb
+++ lib/gcodify/ops/gcodeop.rb
@@ -1,13 +1,18 @@
 require 'gcodify/op'
 
 module Gcodify::Ops
+	# An {Op} for passing through a line (or more) of GCode,
+	# since there are things that haven't been implemented yet
+	# that could be done in raw gcode.
+	# {GcodeOp} passes through a string in the same manner as DummyOp,
+	# but it adds '\n' to the end.
   class GcodeOp < Gcodify::Op
     def initialize(str)
       @str = str
     end
 
     def to_gcode
-      @str
+      @str+'\n'
     end
   end
 end
